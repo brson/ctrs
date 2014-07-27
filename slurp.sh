622facbd2d1ae835117b89e3694b604bec7954f7
Brian Anderson <banderson@mozilla.com>
2014-07-26 17:54:07 -0700

    Slurp aggressively

--- slurp.sh
+++ slurp.sh
@@ -12,4 +12,4 @@
 SRC="$1"
 DST="$2"
 
-find "$SRC" -name "*.rs" -exec ./stab.py "{}" \; 2> /dev/null|grep "\[stable\]"|sed 's/\[stable\] //'|xargs cp -t "$DST"
+find "$SRC" -name "*.rs" -exec ./stab.py "{}" \; 2> /dev/null|grep "\[stable\]"|sed 's/\[stable\] //'|xargs cp -f -t "$DST"
