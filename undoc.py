8ff0e1597c62016d1945267db8d4a664482f320a
Brian Anderson <banderson@mozilla.com>
2014-07-27 16:56:26 -0700

    Add a script for extracting test cases from docs

--- undoc.py
+++ undoc.py
@@ -14,7 +14,66 @@
 import os
 import sys
 
-def undoc(filename, outdir):
+def translate_test_case(out_file_name, markdown_tags, lines):
+
+    is_definitely_rust = True
+
+    useless_tags = [
+        'ignore', 'console', 'text', 'notation',
+        'gram', 'ebnf', 'sh', 'precedence',
+        'field', 'tuple', 'literals',
+        'linked-failure', 'c', 'md', 'keyword',
+        'cpp', 'notrust', 'bash', 'ruby'
+    ]
+
+    # TODO: Do stuff with these
+    usefull_tags = [ 'no_run', 'test_harness' ]
+
+    # Configure translation from tags
+    for tag in markdown_tags:
+        if tag == 'rust':
+            pass
+        elif tag in useless_tags:
+            is_definitely_rust = False
+        elif tag in usefull_tags:
+            is_definitely_rust = False
+        else:
+            # idk!
+            print "NEW TAG DISCOVERED! [" + tag + "]"
+            is_definitely_rust = False
+
+    # Configure translation from source
+
+    has_main = False
+
+    for line in lines:
+        if "fn main" in line:
+            has_main = True
+
+    if not is_definitely_rust:
+        return
+
+    if os.path.exists(out_file_name):
+        print "%s exists!" % out_file_name
+        return
+
+    with open(out_file_name, 'w') as f:
+        if not has_main:
+            f.write("fn main() {\n")
+
+        for line in lines:
+            if line.startswith("# "):
+                line = line[2:len(line)]
+            if not has_main:
+                # Indent to make pretty
+                f.write("    ")
+            f.write(line)
+
+        if not has_main:
+            f.write("}")
+        
+
+def undoc(filename, outdir, test_name):
 
     if not os.path.isfile(filename):
         print "'%s' is not a file" % filename
@@ -23,6 +82,45 @@ def undoc(filename, outdir):
     if not os.path.isdir(outdir):
         print "'%s' is not a dir" % outdir
         return False
+
+    test_num = 0
+
+    with open(filename) as f:
+        in_code_block = False
+        markdown_tags = []
+        lines = []
+        for line in f:
+            if line.startswith("```") or line.startswith("~~~"):
+                in_code_block = not in_code_block
+
+                if in_code_block:
+                    assert not markdown_tags
+                    assert not lines
+                    tags = line
+                    tags = tags.replace("`", "")
+                    tags = tags.replace("~", "")
+                    tags = tags.strip()
+                    tags = tags.translate(None, "{}.")
+                    if len(tags) > 0:
+                        tags = tags.split(",")
+                        if len(tags) == 1:
+                            tags = tags[0].split(" ")
+                    else:
+                        tags = []
+                    markdown_tags = tags
+                else:
+                    out_file_name = outdir + "/" + test_name + "_" + str(test_num).zfill(4)
+                    translate_test_case(out_file_name, markdown_tags, lines)
+                    markdown_tags = []
+                    lines = []
+                    test_num += 1
+
+                continue
+
+            if in_code_block:
+                lines = lines + [line]
+
+            pass
 
     return True
 
@@ -35,7 +133,10 @@ if __name__ == '__main__':
     filename = sys.argv[1]
     outdir = sys.argv[2]
 
-    if undoc(filename, outdir):
+    test_name = os.path.basename(filename)
+    test_name = test_name.replace(".", "_")
+
+    if undoc(filename, outdir, test_name):
         sys.exit(0)
     else:
         sys.exit(1)
