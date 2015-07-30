d17cd0a9c35bce5ebb5c7c756a484bfde306c878
Brian Anderson <banderson@mozilla.com>
2015-07-30 16:57:47 -0700

    Delete dupes

--- test/1.1.0/doc-core/libcore_num_mod_rs_0028.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    assert_eq!((-127i8).checked_sub(1), Some(-128));
-    assert_eq!((-128i8).checked_sub(1), None);
-}
--- test/1.1.0/doc/doc_trpl_documentation_md_0030.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let x = 5;
-    let y = 6;
-    println!("{}", x + y);
-}
